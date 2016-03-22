35b7bb304dcd631531f793fbad7af1d79cf548f3
lbarias <lbarias@ucalgary.ca>
2016-03-22 14:15:13 -0600

    Awesome dropdown menus in soups and salads!

--- VerticalPrototype1/SoupSalads.xaml.cs
+++ VerticalPrototype1/SoupSalads.xaml.cs
@@ -48,5 +48,331 @@ namespace VerticalPrototype1
             }
             
         }
+
+        private void AllergySelect_Click(object sender, RoutedEventArgs e)
+        {
+            if (Allergies.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Hidden;
+            }
+            if (caeserDropDown.Visibility == System.Windows.Visibility.Hidden)
+            {
+                caeserDropDown.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (caeserDropDown.Visibility == System.Windows.Visibility.Visible)
+            {
+                caeserDropDown.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void GlutenFree_Click(object sender, RoutedEventArgs e)
+        {
+            if (Allergies.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void PF_Click(object sender, RoutedEventArgs e)
+        {
+            if (Allergies.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Vegan_Click(object sender, RoutedEventArgs e)
+        {
+            if (Allergies.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Paleo_Click(object sender, RoutedEventArgs e)
+        {
+            if (Allergies.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void AllergySelect_Click1(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_1.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_1.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Hidden;
+            }
+            if (caeserDropDown_1.Visibility == System.Windows.Visibility.Hidden)
+            {
+                caeserDropDown_1.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (caeserDropDown_1.Visibility == System.Windows.Visibility.Visible)
+            {
+                caeserDropDown_1.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void HouseSalad_Click(object sender, RoutedEventArgs e)
+        {
+            if (caeserDropDown_1.Visibility == System.Windows.Visibility.Hidden)
+            {
+                caeserDropDown_1.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (caeserDropDown_1.Visibility == System.Windows.Visibility.Visible)
+            {
+                caeserDropDown_1.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void GlutenFree_Click1(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_1.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_1.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void PF_Click1(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_1.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_1.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Vegan_Click1(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_1.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_1.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Paleo_Click1(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_1.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_1.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_1.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+        private void GlutenFree_Click2(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_2.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_2.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void PF_Click2(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_2.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_2.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Vegan_Click2(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_2.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_2.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Paleo_Click2(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_2.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_2.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void AllergySelect_Click2(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_2.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_2.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_2.Visibility = System.Windows.Visibility.Hidden;
+            }
+            if (ClamSoupDropDown.Visibility == System.Windows.Visibility.Hidden)
+            {
+                ClamSoupDropDown.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (ClamSoupDropDown.Visibility == System.Windows.Visibility.Visible)
+            {
+                ClamSoupDropDown.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void clamChowder_Click(object sender, RoutedEventArgs e)
+        {
+            if (ClamSoupDropDown.Visibility == System.Windows.Visibility.Hidden)
+            {
+                ClamSoupDropDown.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (ClamSoupDropDown.Visibility == System.Windows.Visibility.Visible)
+            {
+                ClamSoupDropDown.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void AllergySelect3_Click(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_3.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_3.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Hidden;
+            }
+            if (TomatoSoupDropDown.Visibility == System.Windows.Visibility.Hidden)
+            {
+                TomatoSoupDropDown.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (TomatoSoupDropDown.Visibility == System.Windows.Visibility.Visible)
+            {
+                TomatoSoupDropDown.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void GlutenFree_Click3(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_3.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_3.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void PF_Click3(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_3.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_3.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Vegan_Click3(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_3.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_3.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void Paleo_Click3(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_3.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_3.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+        private void AllergySelect_Click3(object sender, RoutedEventArgs e)
+        {
+            if (Allergies_3.Visibility == System.Windows.Visibility.Hidden)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (Allergies_3.Visibility == System.Windows.Visibility.Visible)
+            {
+                Allergies_3.Visibility = System.Windows.Visibility.Hidden;
+            }
+            if (TomatoSoupDropDown.Visibility == System.Windows.Visibility.Hidden)
+            {
+                TomatoSoupDropDown.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (TomatoSoupDropDown.Visibility == System.Windows.Visibility.Visible)
+            {
+                TomatoSoupDropDown.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        private void TomatoSoup_Click(object sender, RoutedEventArgs e)
+        {
+            if (TomatoSoupDropDown.Visibility == System.Windows.Visibility.Hidden)
+            {
+                TomatoSoupDropDown.Visibility = System.Windows.Visibility.Visible;
+            }
+            else if (TomatoSoupDropDown.Visibility == System.Windows.Visibility.Visible)
+            {
+                TomatoSoupDropDown.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
     }
 }
