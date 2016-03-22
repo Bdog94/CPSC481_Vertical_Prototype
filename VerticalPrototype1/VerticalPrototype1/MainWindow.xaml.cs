0a78cfc010e2ddc5f72450a4f7f245bd829b3a9d
lbarias <lbarias@ucalgary.ca>
2016-03-22 09:14:04 -0600

    Vertical Prototype add

---
+++ VerticalPrototype1/VerticalPrototype1/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VerticalPrototype1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static MainWindow m;
+        public MainWindow()
+        {
+            m = this;
+            InitializeComponent();
+            SelectTable lw = new SelectTable();
+            stackPanel.Children.Add(lw);
+        }
+
+        public static void switchToTableView()
+        {
+            SelectTable st = new SelectTable();
+            m.switchTo(st);
+        }
+
+        private void switchTo(UserControl u)
+        {
+            stackPanel.Children.Clear();
+            stackPanel.Children.Add(u);
+        }
+
+        public static void switchToMenuCategoriesView()
+        {
+            MenuCategories mc = new MenuCategories();
+            m.switchTo(mc);
+        }
+        public static void switchToSoupSalad()
+        {
+            SoupSalads mc = new SoupSalads();
+            m.switchTo(mc);
+        }
+
+    }
+}
---
+++ VerticalPrototype1/VerticalPrototype1/MenuCategories.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VerticalPrototype1
+{
+    /// <summary>
+    /// Interaction logic for MenuCategories.xaml
+    /// </summary>
+    public partial class MenuCategories : UserControl
+    {
+        //static MainWindow m;
+        public MenuCategories()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectTableButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.switchToTableView();
+        }
+
+        private void btnSoupSalad_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.switchToSoupSalad();
+        }
+    }
+}
