67a4b74255fa1ac73035d95496cf1ec6d77a18a5
Bernie Mayer <bemayer@ucalgary.ca>
2016-03-23 13:47:49 -0600

    Fixed some problems

---
+++ VerticalPrototype1/CurrentOrder.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VerticalPrototype1
+{
+    /// <summary>
+    /// Interaction logic for CurrentOrder.xaml
+    /// </summary>
+    public partial class CurrentOrder : UserControl
+    {
+        public static StackPanel foodPanel;
+        public static ScrollViewer scroller;
+        public CurrentOrder()
+        {
+            InitializeComponent();
+            foodPanel= stackPanel;
+            scroller = svFoodItems;
+        }
+
+        public void addFoodItem(FoodItem f)
+        {
+            foodPanel.Children.Add(f);
+        }
+    }
+}
---
+++ VerticalPrototype1/FoodItem.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VerticalPrototype1
+{
+    /// <summary>
+    /// Interaction logic for FoodItem.xaml
+    /// </summary>
+    public partial class FoodItem : UserControl
+    {
+        private string foodItemName;
+
+        public string FoodItemName
+        {
+            get { return foodItemName; }
+            set { foodItemName = value;
+            lblFoodName.Content = foodItemName;
+            }
+        }
+
+        private string foodCustomizations;
+
+        public string FoodCustomizations
+        {
+            get { return foodCustomizations; }
+            set { foodCustomizations = value;
+            lblFoodName.Content = foodCustomizations;
+            }
+        }
+
+
+        public FoodItem()
+        {
+            InitializeComponent();
+        }
+    }
+}
