82d61a459907a0d692af18c77bcbc57e4d9aedc2
Bernie Mayer <bemayer@ucalgary.ca>
2016-03-24 16:59:37 -0600

    Previous commit would cause problems

---
+++ VerticalPrototype1/PastOrders.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VerticalPrototype1
+{
+    /// <summary>
+    /// Interaction logic for PastOrders.xaml
+    /// </summary>
+    public partial class PastOrders : UserControl
+    {
+        public PastOrders()
+        {
+            InitializeComponent();
+            Label lbl = new Label();
+            lbl.Content = "Test";
+            stackPanel.Children.Add(lbl);
+        }
+
+        private void BackToMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow.switchToMenuCategoriesView();
+        }
+    }
+}
